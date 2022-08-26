dae0f0627710a29bb0d341c6589130afbc46d308
Samoge <samuelokwuchukwu4@gmail.com>
2022-08-26 02:33:34 +0100

    DX-Explore by SamOge

--- Resources/main.js
+++ Resources/main.js
@@ -2,7 +2,7 @@ var hamburger = document.querySelector('.hamburger-menu')
 var mobilelinks = document.querySelector('.mobile_link-holder')
 var backdrop = document.querySelector('.backdrop')
 
-function Addclass() {
+function Addclass(){
     hamburger.classList.toggle('show')
     mobilelinks.classList.toggle('showlinks')
     backdrop.classList.toggle('showlinks')
